1ddc67e2c0fe46ae0b01e40eebfd3bae4835ff8e
yorksk <37493589+yorksk@users.noreply.github.com>
2019-03-12 22:08:11 -0600

    Add files via upload

---
+++ NoTransparency.js
@@ -0,0 +1,66 @@
+﻿/**
+* @@@BUILDINFO@@@ NoTransparency.js !Version! Tue Mar 12 2019
+*/
+//Run the script in Isolation or Symbol Editing Mode
+//It makes a copy of dimmed items in locked "TEMP" layer with opacity = 100
+// DELETE THE "TEMP" LAYER BEFORE EXIT THE MODE
+
+
+function main() {
+    
+    var doc = app.activeDocument;
+    var editedLayer = doc.layers[0];
+    var lockedLayer = doc.layers[1];
+    var lll = lockedLayer.layers.length;
+    var elName = editedLayer.name;
+    var toLayer;
+    
+    if (elName == "Isolation Mode" || elName == "Symbol Editing Mode") {
+        switch(editedLayer.name) {
+            case "Isolation Mode":
+                if (editedLayer.layers.length > 0 && editedLayer.layers[0].name == "TEMP") {
+                    alert("Check \"TEMP\" Layer!");
+                    break;
+                }
+                alert("Delete TEMP Layer before\nYou exit THIS mode!");
+                editedLayer.layers.add();
+                editedLayer.layers[0].name = "TEMP";
+                editedLayer.layers[0].zOrder(ZOrderMethod.SENDTOBACK);
+                toLayer = editedLayer.layers[0];
+                moveToTemp(toLayer);
+                break;
+            case "Symbol Editing Mode":
+                if (editedLayer.layers[0].layers.length > 0 && editedLayer.layers[0].layers[0].name == "TEMP") {
+                    alert("Check \"TEMP\" Layer!");
+                    break;
+                }
+                alert("Delete TEMP Layer before\nYou exit THIS mode!");
+                editedLayer.layers[0].layers.add();
+                editedLayer.layers[0].layers[0].name = "TEMP";
+                editedLayer.layers[0].layers[0].zOrder(ZOrderMethod.SENDTOBACK);
+                toLayer = editedLayer.layers[0].layers[0];
+                moveToTemp(toLayer);
+                break;
+            default:
+                alert("It doesn't lock like\nIsolated or Symbol\nEditing Mode.");
+        }
+    } else {
+        alert("It doesn't look like\nIsolated or Symbol\nEditing Mode.");
+    }
+    
+        
+    function moveToTemp(layer) {
+        for (var i = lll-1; i >= 0; i--) {
+            var inLayer = lockedLayer.layers[i].pageItems.length;
+            if (inLayer > 0) {
+                for (var j = inLayer - 1; j >= 0; j--) {
+                    lockedLayer.layers[i].pageItems[j].duplicate(layer);
+                }
+            }
+        }
+        layer.locked = true;
+    }
+    
+}
+main();
+
